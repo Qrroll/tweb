fd5a353a8ff9a92eb98ab6da62c2e48f64df0029
Qrroll <dainsureivu@gmail.com>
2023-03-21 20:18:50 +0200

    add/layout/FooterCustom

--- tweb/src/App.tsx
+++ tweb/src/App.tsx
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
-import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-} from '@ant-design/icons';
 import { Layout, theme } from 'antd';
 import {SideBarCustom} from "./layout/SideBarCustom/SideBarCustom";
 import {HeaderCustom} from "./layout/HeaderCustom/HeaderCustom";
-import {Footer} from "antd/lib/layout/layout";
+import {FooterCustom} from "./layout/FooterCustom/FooterCustom";
 
 const { Content } = Layout;
 
@@ -21,21 +14,23 @@ const App: React.FC = () => {
 
     const [sideBarIsOpen, setSideBarOpen] = useState(true);
 
-
     return (
         <Layout>
+
             <SideBarCustom sideBarIsOpen={sideBarIsOpen} setSideBarOpen={setSideBarOpen}/>
 
             <Layout className="site-layout">
-                <HeaderCustom sideBarIsOpen={sideBarIsOpen} setSideBarOpen={setSideBarOpen}/>
 
+                <HeaderCustom sideBarIsOpen={sideBarIsOpen} setSideBarOpen={setSideBarOpen}/>
 
                 <Content style={{ margin: '24px 16px 0' }}>
                     <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>content</div>
                 </Content>
 
-                <Footer style={{ textAlign: 'center' }}>Kek Design ©2023 Created by Ant UED</Footer>
+                <FooterCustom />
+
             </Layout>
+
         </Layout>
     );
 };
---
+++ tweb/src/layout/FooterCustom/FooterCustom.tsx
@@ -0,0 +1,12 @@
+import {Layout} from "antd";
+import React from "react";
+
+const { Footer } = Layout;
+export const FooterCustom = () => {
+
+    return (
+        <Footer style={{ textAlign: 'center' }}>
+            Kek Design ©2023 Created by Qrroll
+        </Footer>
+    )
+}
